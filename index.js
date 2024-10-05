e6acaa7e61d4781092e083eadb76f7faa163eb18
amuni5323 <156849494+amuni5323@users.noreply.github.com>
2024-10-05 21:32:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Number One: 
+function add(a, b){
+    return a + b;
+}
+// Answer
+const add = (a, b) => a + b;
+
+// Number Two
+function multiply(a, b){
+    return a * b;
+}
+// Answer
+const multiply = (a, b) => a * b;
+
+// Number Three
+function greet(name){
+    return 'hello' + name;
+}
+
+// Answer
+const greet = (name) => `hello ${name}`; 
